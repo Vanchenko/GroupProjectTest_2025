563f8c99206b50a179963b7424457172644ede77
Yevhen <vanchenkoev@pakline.com.ua>
2025-08-14 11:09:33 +0300

    next update

--- src/main.js
+++ src/main.js
@@ -1,22 +1,3 @@
-// (() => {
-//   const refs = {
-//     // Додати атрибут data-modal-open на кнопку відкриття
-//     openModalBtn: document.querySelector('[data-menu-open]'),
-//     // Додати атрибут data-modal-close на кнопку закриття
-//     closeModalBtn: document.querySelector('[data-menu-close]'),
-//     // Додати атрибут data-modal на бекдроп модалки
-//     modal: document.querySelector('[data-menu]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-//     refs.modal.classList.toggle('is-open');
-//   }
-// })();
-
 (() => {
   const refs = {
     // Додати атрибут data-modal-open на кнопку відкриття
@@ -33,7 +14,5 @@
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle('is-open');
-    console.log(document.querySelector('[data-modal]'))
-    console.log(refs.closeModalBtn)
   }
 })();
